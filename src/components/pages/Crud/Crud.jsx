ad1a94bb83e3bf96be0d11db3348385c97af9c1e
aledc7 <alejandrocdecastro@gmail.com>
2022-02-12 17:13:34 -0300

    crud avance 2

--- src/components/pages/Crud/Crud.jsx
+++ src/components/pages/Crud/Crud.jsx
@@ -9,7 +9,9 @@ import './crud.css';
 function Crud() {
     return (
         <div className="main">
-            <h2 className="main-header">React Crud Operations</h2>
+            <div className="main">
+                <h2 className="main-header">React Crud Operations</h2>
+            </div>
         </div>
     )
 }
